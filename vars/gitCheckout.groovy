d388e7b5fe1c7265d6d95c7a2258fd8e3fbc16f4
vishwaranjanpandey <vishwaranjan.p12@gmail.com>
2024-09-09 10:05:30 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,3 @@
 def call(Map stageParams) {
-checkout scmGit(branches: [[name: stageParams.branch]], extensions: [], userRemoteConfigs: [[url: stageParams.url ]])
+checkout scmGit(branches: [[name: stageParams.branch]], userRemoteConfigs: [[url: stageParams.url ]])
 }
