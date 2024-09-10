01aa638fdfc455a97924cacf34c9c988299162a2
vishwaranjanpandey <vishwaranjan.p12@gmail.com>
2024-09-10 09:11:50 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
-def call (String ecr_RepoName, String aws_account_ID, String region ) {
+def call (String aws_account_ID, String region, String ecr_RepoName, ) {
   sh """
   docker build -t ${ecr_RepoName} .
   docker tag ${ecr_RepoName}:latest ${aws_account_ID}.dkr.ecr.${region}.amazonaws.com/${ecr_RepoName}:latest
