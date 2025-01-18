d84a634d549b448893d403e164c1da0a23572633
vishwaranjanpandey <vishwaranjan.p12@gmail.com>
2025-01-18 18:38:10 +0530

    Update nexusDeploy.groovy

--- vars/nexusDeploy.groovy
+++ vars/nexusDeploy.groovy
@@ -1,5 +1,5 @@
 def call () {
-   withMaven(globalMavenSettingsConfig: '561d4998-105b-4bf9-89b2-14fed2889b59', jdk: 'jdk17', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
+   withMaven(globalMavenSettingsConfig: 'Maven-setting', jdk: 'jdk17', maven: 'maven3', mavenSettingsConfig: '', traceability: true) {
    sh "mvn deploy -DskipTests=true"
    }
 }
